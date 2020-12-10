d3916bd5f1e7f4f465f399649776a7f4c72de210
Raul Arrieta <raul.arrieta@gmail.com>
2020-12-10 17:48:37 +0100

    Create different classes per each item

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,44 +1,25 @@
 # -*- coding: utf-8 -*-
-
 SULFURAS = "Sulfuras, Hand of Ragnaros"
 BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
 AGED_BRIE = "Aged Brie"
 
 class GildedRose(object):
-    def is_aged_brie(self, item):
-        return item.name == AGED_BRIE
 
-    def is_backstage_pass(self, item):
-        return item.name == BACKSTAGE_PASS
+    def item_factory(self, item):
+        allowedItems = {
+            "Backstage passes to a TAFKAL80ETC concert": BackstagePassItem,
+            "Aged Brie": AgedBrieItem,
+            "Sulfuras, Hand of Ragnaros": SulfurasItem,
+        }
+
+        for type_name, class_ in allowedItems.items():
+            if type_name in item.name:
+                return class_(item.name, item.sell_in, item.quality)
+
+        return RegularItem(item.name, item.sell_in, item.quality)
 
     def is_sulfuras(self, item):
         return item.name == SULFURAS
-
-    def get_updated_quality_for_backstage_pass(self, item):
-        quality = item.quality + 1
-        if item.sell_in < 0:
-            quality = 0
-        elif item.sell_in < 5:
-            quality += 2
-        elif item.sell_in < 10:
-            quality += 1
-                        
-        return min([50, quality])
-
-    def get_updated_quality_for_aged_brie(self, item):
-        if item.sell_in < 0 & item.quality < 49:
-            item.quality += 2
-        else:
-            item.quality += 1
-            
-        return min([50, item.quality])
-
-    def get_updated_quality_regular_item(self, item):
-        if item.sell_in < 0:
-            item.quality -= 2
-        else:
-            item.quality -= 1
-        return max([0, item.quality])
 
     def get_updated_sell_in(self, item):
         return item.sell_in if self.is_sulfuras(item) else item.sell_in - 1
@@ -50,17 +31,8 @@ class GildedRose(object):
         for item in self.items:
             item.sell_in = self.get_updated_sell_in(item)
 
-            if self.is_aged_brie(item):
-                item.quality = self.get_updated_quality_for_aged_brie(item)
+            item.quality = self.item_factory(item).get_updated_quality()
 
-            elif self.is_backstage_pass(item):
-                item.quality = self.get_updated_quality_for_backstage_pass(item)
-
-            elif self.is_sulfuras(item):
-                pass
-
-            else:
-                item.quality = self.get_updated_quality_regular_item(item)
 
 class Item:
     def __init__(self, name, sell_in, quality):
@@ -70,3 +42,37 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+class AgedBrieItem(Item):
+    def get_updated_quality(self):
+        if self.sell_in < 0 & self.quality < 49:
+            self.quality += 2
+        else:
+            self.quality += 1
+
+        return min([50, self.quality])
+
+class BackstagePassItem(Item):
+    def get_updated_quality(self):
+        quality = self.quality + 1
+        if self.sell_in < 0:
+            quality = 0
+        elif self.sell_in < 5:
+            quality += 2
+        elif self.sell_in < 10:
+            quality += 1
+
+        return min([50, quality])
+
+class SulfurasItem(Item):
+    def get_updated_quality(self):
+        return self.quality
+
+class RegularItem(Item):
+
+    def get_updated_quality(self):
+        if self.sell_in < 0:
+            self.quality -= 2
+        else:
+            self.quality -= 1
+        return max([0, self.quality])
